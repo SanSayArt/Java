4de67384a3d259e3b7dc1dcf2b79464aa79baf37
SanSayArt <SanSayArt@gmail.com>
2024-01-30 09:43:01 +0500

    commit 123

---
+++ Lesson_1/Lesson1_work1.java
@@ -0,0 +1,20 @@
+package Lesson_1;
+
+/**
+ * Lesson1_work1
+ * 
+ */
+public class Lesson1_work1 {
+    public static void main(String[] args) {
+        float f = 123.45f;
+
+        String str = "тест";
+        System.out.println("bye world. - " + str + "  " + f);
+        System.out.println("Проверка");
+        int a = 3;
+        int res = a -- - -- a;
+        System.out.println(res);
+
+    }
+    
+}
